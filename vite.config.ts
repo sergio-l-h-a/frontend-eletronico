3f89cd2de7804293923862e02684aa7e8f7c87d4
Francisco-Sergio <lanhousearte@outlook.com>
2026-09-22 22:59:37 -0300

    fix: corrigir URL

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Eleva o limite do aviso de 500kB para 1000kB
+    rollupOptions: {
+      output: {
+        // Separa as dependências de node_modules (como React e React Router) em chunks menores
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
 })
